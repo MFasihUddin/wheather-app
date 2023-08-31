f2035ea4eb747f18716aa11af8c6d40c8ee9df4b
fasih <fasih0070@gmail.com>
2023-08-31 09:26:06 +0500

    using request package

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const request = require("req");
+const request = require("request");
 
-const url = "";
+const API_KEY = "22cfb203f26b4e9c9c08f8b37fec6d98";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`;
 
 request({ url: url }, (error, response) => {
   console.log(response);
